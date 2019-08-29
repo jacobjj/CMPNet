7466b3e4dfe1a2aac5c57869fed4887c9b158d41
jacobjj <jjj025@ucsd.edu>
2019-08-28 17:47:04 -0700

    added neuralPlan method

--- src/mpnet_plan.py
+++ src/mpnet_plan.py
@@ -1,5 +1,7 @@
 import torch
+import numpy as np
 from mpnet import MPnetBase
+import copy
 
 
 class MPNetPlan(MPnetBase):
@@ -14,7 +16,7 @@ class MPNetPlan(MPnetBase):
         # Load Model file
         self.load_network_parameters(modelFile)
 
-    def getPlan(self, IsInCollision, start, goal, obs):
+    def getPath(self, IsInCollision, start, goal, obs):
         """
         Generate a plan using the MPnet
         : param IsInCollision :
@@ -24,6 +26,7 @@ class MPNetPlan(MPnetBase):
         : return path list: key points to generate a path
         """
         obs = torch.from_numpy(obs)
+        # NOTE: Do we need to convert them here or do we need to convert them only while  executing in the network
         path = [
             torch.from_numpy(start).type(torch.FloatTensor),
             torch.from_numpy(goal).type(torch.FloatTensor)
@@ -41,18 +44,30 @@ class MPNetPlan(MPnetBase):
             elif (t > 3):
                 step_sz = step_sz / 2
 
-            # neural_replan is used to both generate the initial paths, and replan between points that are in collision
-            path, _ = neural_replan(self.mpNet,
-                                    path,
-                                    obc,
-                                    obs,
-                                    IsInCollision,
-                                    normalize_func,
-                                    unnormalize_func,
-                                    t == 0,
-                                    step_sz=step_sz,
-                                    time_flag=time_flag,
-                                    steerTo=steerTo)
+            for j, _ in enumerate(path[:-1]):
+                steer = self.steerTo(path[marker], path[marker + 1],
+                                     IsInCollision)
+                if not steer:
+                    marker = j
+                    # Remove the two points
+                    startNode = path.pop(marker)
+                    goalNode = path.pop(marker)
+                    break
+
+            # Generate a plan
+            if not steer:
+                miniPath, _ = self.neuralPlan(startNode,
+                                              goalNode,
+                                              obs,
+                                              IsInCollision,
+                                              maxPoints=50)
+
+                # Remove points that are in collision
+                if len(miniPath) > 2:
+                    miniPath = self.removeCollision(miniPath, IsInCollision)
+                    # append miniPath to path
+                    for i, point in miniPath:
+                        path.insert(marker + i, point)
 
             # lazy vertex contraction
             path = self.lvc(path, IsInCollision)
@@ -60,7 +75,36 @@ class MPNetPlan(MPnetBase):
             if self.feasibility_check(path, IsInCollision):
                 print('feasible, ok!')
                 return path
+
         return []
+
+    def neuralPlan(self, start, goal, obs, IsInCollision, maxPoints):
+        """
+        Generate a path using MPnet
+        """
+        pA = [start]
+        pB = [goal]
+        noPath = [copy.copy(start), copy.copy(goal)]
+        tree = 0
+        for _ in range(maxPoints):
+            if tree == 0:
+                network_input = np.concatenate((start, goal), axis=1)
+                tInput = self.format_input(obs, network_input)
+                start = self.mpNet(tInput).squeeze(0).data.cpu()
+                start = self.denormalize(start)
+                pA.append(start)
+                tree = 1
+            else:
+                network_input = np.concatenate((goal, start), axis=1)
+                tInput = self.format_input(obs, network_input)
+                goal = self.mpNet(tInput).squeeze(0).data.cpu()
+                goal = self.denormalize(goal)
+                pB.append(goal)
+                tree = 0
+            target_reached = self.steerTo(start, goal, IsInCollision)
+            if target_reached:
+                return pA.extend(reversed(pB)), 0
+        return noPath, 0
 
     def lvc(self, path, IsInCollision):
         """
@@ -84,11 +128,23 @@ class MPNetPlan(MPnetBase):
         return path
 
     def feasibility_check(self, path, IsInCollision):
-        # checks the feasibility of entire path including the path edges
-        # by checking for each adjacent vertices
+        """
+         checks the feasibility of entire path including the path edge by checking for each adjacent vertices
+        """
         for i, _ in enumerate(path[:-1]):
             if not self.steerTo(
                     path[i], path[i + 1], IsInCollision, step_sz=self.stepSz):
                 # collision occurs from adjacent vertices
                 return 0
         return 1
+
+    def removeCollision(self, path, IsInCollision):
+        """
+        Rule out nodes that are already in collision
+        """
+        new_path = []
+
+        for i in range(0, len(path)):
+            if not IsInCollision(path[i].numpy()):
+                new_path.append(path[i])
+        return new_path
