51a72f4c4db4aa606b8fe755023f99bf3e6d4b6e
jacobjj <jjj025@ucsd.edu>
2019-09-03 17:34:28 -0700

    pep8 formatting

--- MPnet/src/utility.py
+++ MPnet/src/utility.py
@@ -2,10 +2,12 @@ import torch
 from torch.autograd import Variable
 import copy
 
+
 def to_var(x, volatile=False):
     if torch.cuda.is_available():
         x = x.cuda()
     return Variable(x, volatile=volatile)
+
 
 def save_state(net, torch_seed, np_seed, py_seed, fname):
     # save both model state and optimizer state
@@ -18,15 +20,25 @@ def save_state(net, torch_seed, np_seed, py_seed, fname):
     }
     torch.save(states, fname)
 
+
 def load_net_state(net, fname):
-    checkpoint = torch.load(fname, map_location='cuda:%d' % (torch.cuda.current_device()))
+    checkpoint = torch.load(fname,
+                            map_location='cuda:%d' %
+                            (torch.cuda.current_device()))
     net.load_state_dict(checkpoint['state_dict'])
 
+
 def load_opt_state(net, fname):
-    checkpoint = torch.load(fname, map_location='cuda:%d' % (torch.cuda.current_device()))
+    checkpoint = torch.load(fname,
+                            map_location='cuda:%d' %
+                            (torch.cuda.current_device()))
     net.opt.load_state_dict(checkpoint['optimizer'])
+
 
 def load_seed(fname):
     # load both torch random seed, and numpy random seed
-    checkpoint = torch.load(fname, map_location='cuda:%d' % (torch.cuda.current_device()))
-    return checkpoint['torch_seed'], checkpoint['np_seed'], checkpoint['py_seed']
+    checkpoint = torch.load(fname,
+                            map_location='cuda:%d' %
+                            (torch.cuda.current_device()))
+    return checkpoint['torch_seed'], checkpoint['np_seed'], checkpoint[
+        'py_seed']
