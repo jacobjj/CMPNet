7dc858676d9f2b7fa449f9e4a48712d114ff1bb6
jacobjj <jjj025@ucsd.edu>
2019-09-03 17:38:48 -0700

    reduced number of layers

--- MPnet/Model/model.py
+++ MPnet/Model/model.py
@@ -5,29 +5,41 @@ from torch.nn.utils.rnn import pack_padded_sequence
 from torch.autograd import Variable
 
 
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        # nn.init.xavier_normal_(m.weight.data)
+        m.weight.data.normal_(0.0, 1e-3)
+    elif classname.find('Linear') != -1:
+        # nn.init.xavier_normal_(m.weight.data)
+        m.weight.data.normal_(0.0, 1e-3)
+        m.bias.data.fill_(0)
+
+
 class MLP(nn.Module):
     def __init__(self, input_size, output_size):
         super(MLP, self).__init__()
         self.fc = nn.Sequential(
-            nn.Linear(input_size, 1280),
-            nn.PReLU(),
-            nn.Dropout(),
-            nn.Linear(1280, 1024),
-            nn.PReLU(),
-            nn.Dropout(),
-            nn.Linear(1024, 896),
-            nn.PReLU(),
-            nn.Dropout(),
-            nn.Linear(896, 768),
-            nn.PReLU(),
-            nn.Dropout(),
-            nn.Linear(768, 512),
-            nn.PReLU(),
-            nn.Dropout(),
-            nn.Linear(512, 384),
-            nn.PReLU(),
-            nn.Dropout(),
-            nn.Linear(384, 256),
+            # nn.Linear(input_size, 1280),
+            # nn.PReLU(),
+            # nn.Dropout(),
+            # nn.Linear(1280, 1024),
+            # nn.PReLU(),
+            # nn.Dropout(),
+            # nn.Linear(1024, 896),
+            # nn.PReLU(),
+            # nn.Dropout(),
+            # nn.Linear(896, 768),
+            # nn.PReLU(),
+            # nn.Dropout(),
+            # nn.Linear(768, 512),
+            # nn.PReLU(),
+            # nn.Dropout(),
+            # nn.Linear(512, 384),
+            # nn.PReLU(),
+            # nn.Dropout(),
+            # nn.Linear(384, 256),
+            nn.Linear(input_size, 256),
             nn.PReLU(),
             nn.Dropout(),
             nn.Linear(256, 256),
@@ -43,6 +55,7 @@ class MLP(nn.Module):
             nn.PReLU(),
             nn.Linear(32, output_size),
         )
+        self.fc.apply(weights_init)
 
     def forward(self, x):
         out = self.fc(x)
