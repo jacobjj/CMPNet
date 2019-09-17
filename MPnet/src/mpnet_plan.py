05d7e8ce6efd62009555567b2ee5b7fa9095d63e
jacobjj <jjj025@ucsd.edu>
2019-09-17 15:26:58 -0700

    added code for motion planning given a learned network model

--- MPnet/src/mpnet_plan.py
+++ MPnet/src/mpnet_plan.py
@@ -1,22 +1,58 @@
 import torch
 import numpy as np
-from mpnet import MPnetBase
+from src.mpnet import MPnetBase
 import copy
+import dubins
+import matplotlib.pyplot as plt
 
 
 class MPNetPlan(MPnetBase):
     def __init__(self, modelFile, steerTo, stepSz=0.01, **kwargs):
         """
         A class that plans a path for a given environment and observation
+        TODO : Pickle BaseClass, making it easier for loading and running models
         """
-        super().__init__(self, **kwargs)
+        super().__init__(**kwargs)
 
         self.stepSz = stepSz
         self.steerTo = steerTo
         # Load Model file
         self.load_network_parameters(modelFile)
 
-    def getPath(self, IsInCollision, start, goal, obs):
+    def plotMainPath(self, ax, path, pointCloud):
+        """
+        A function to plot the point cloud information and path
+        """
+        ax.scatter(pointCloud[:, 0], pointCloud[:, 1], color='c')
+        if path:
+            ax.scatter(path[0][0], path[0][1], marker='o', color='r')
+            ax.scatter(path[-1][0], path[-1][1], marker='o', color='g')
+            self.plotPoints(ax, path[1:-1], color='m')
+
+    def plotPoints(self, ax, path, color='r'):
+        """
+        Plot the points in path
+        """
+        for p in path:
+            ax.scatter(p[0], p[1], marker='x', color=color)
+
+    def getDubinsTraj(self, path):
+        """
+        Return the dubin trajectory of the current path
+        """
+        traj = []
+        for i, _ in enumerate(path[:-1]):
+            p = dubins.shortest_path(tuple(path[i]), tuple(path[i + 1]), d=0.6)
+            configs, _ = p.sample_many(0.1)
+            traj.extend(configs)
+        return traj
+
+    def plotDubinPath(self, ax, path):
+        traj = self.getDubinsTraj(path)
+        traj = np.array(traj)
+        ax.plot(traj[:, 0], traj[:, 1])
+
+    def getPath(self, IsInCollision, start, goal, obs, pointCloud=None):
         """
         Generate a plan using the MPnet
         : param IsInCollision :
@@ -27,14 +63,19 @@ class MPNetPlan(MPnetBase):
         """
         obs = torch.from_numpy(obs)
         # NOTE: Do we need to convert them here or do we need to convert them only while  executing in the network
-        path = [
-            torch.from_numpy(start).type(torch.FloatTensor),
-            torch.from_numpy(goal).type(torch.FloatTensor)
-        ]
-
+        # path = [
+        #     torch.from_numpy(start).type(torch.FloatTensor),
+        #     torch.from_numpy(goal).type(torch.FloatTensor)
+        # ]
+        path = [start, goal]
+        fig, (axMain, axMini) = plt.subplots(1, 2)
+        for ax in (axMain, axMini):
+            ax.set_xlim([-5, 5])
+            ax.set_ylim([-5, 5])
+        self.plotMainPath(axMain, path, pointCloud)
         # Number of re-planning steps taken.
         MAX_NEURAL_REPLAN = 6
-
+        step_sz = 0.01
         for t in range(MAX_NEURAL_REPLAN):
             # adaptive step size on replanning attempts
             if (t == 2):
@@ -44,9 +85,9 @@ class MPNetPlan(MPnetBase):
             elif (t > 3):
                 step_sz = step_sz / 2
 
+            print("Plan {}".format(t))
             for j, _ in enumerate(path[:-1]):
-                steer = self.steerTo(path[marker], path[marker + 1],
-                                     IsInCollision)
+                steer = self.steerTo(path[j], path[j + 1], IsInCollision)
                 if not steer:
                     marker = j
                     # Remove the two points
@@ -61,13 +102,35 @@ class MPNetPlan(MPnetBase):
                                               obs,
                                               IsInCollision,
                                               maxPoints=50)
+                import pdb
+                pdb.set_trace()
+                self.plotMainPath(axMini, [], pointCloud)
+                self.plotPoints(axMini, miniPath)
+                plt.pause(1)
 
+                print("Remove points in Collision")
                 # Remove points that are in collision
                 if len(miniPath) > 2:
                     miniPath = self.removeCollision(miniPath, IsInCollision)
-                    # append miniPath to path
-                    for i, point in miniPath:
-                        path.insert(marker + i, point)
+
+                axMini.clear()
+                self.plotPoints(axMini, miniPath)
+                self.plotMainPath(axMini, [], pointCloud)
+                for ax in (axMain, axMini):
+                    ax.set_xlim([-5, 5])
+                    ax.set_ylim([-5, 5])
+                plt.pause(1)
+
+                # append miniPath to path
+                for i, point in enumerate(miniPath):
+                    path.insert(marker + i, point)
+
+                axMain.clear()
+                self.plotMainPath(axMain, path, pointCloud)
+                for ax in (axMain, axMini):
+                    ax.set_xlim([-5, 5])
+                    ax.set_ylim([-5, 5])
+                plt.pause(2)
 
             # lazy vertex contraction
             path = self.lvc(path, IsInCollision)
@@ -78,32 +141,44 @@ class MPNetPlan(MPnetBase):
 
         return []
 
+    def formatInput(self, v):
+        if len(v.shape) == 1:
+            return np.expand_dims(v, 0)
+
     def neuralPlan(self, start, goal, obs, IsInCollision, maxPoints):
         """
         Generate a path using MPnet
         """
+        # start = self.formatInput(start)
+        # goal = self.formatInput(goal)
         pA = [start]
         pB = [goal]
         noPath = [copy.copy(start), copy.copy(goal)]
         tree = 0
         for _ in range(maxPoints):
             if tree == 0:
-                network_input = np.concatenate((start, goal), axis=1)
-                tInput = self.format_input(obs, network_input)
-                start = self.mpNet(tInput).squeeze(0).data.cpu()
-                start = self.denormalize(start)
+                network_input = np.concatenate((start, goal))
+                network_input = self.formatInput(network_input)
+                tobs, tInput = self.format_input(obs, network_input)
+                start = self.mpNet(tInput, tobs).squeeze().data.cpu()
+                start = self.denormalize(start, self.worldSize).numpy()
                 pA.append(start)
                 tree = 1
             else:
-                network_input = np.concatenate((goal, start), axis=1)
-                tInput = self.format_input(obs, network_input)
-                goal = self.mpNet(tInput).squeeze(0).data.cpu()
-                goal = self.denormalize(goal)
+                network_input = np.concatenate((goal, start))
+                network_input = self.formatInput(network_input)
+                tobs, tInput = self.format_input(obs, network_input)
+                goal = self.mpNet(tInput, tobs).squeeze(0).data.cpu()
+                goal = self.denormalize(goal, self.worldSize).numpy()
                 pB.append(goal)
                 tree = 0
-            target_reached = self.steerTo(start, goal, IsInCollision)
+
+            target_reached = self.steerTo(start.squeeze(), goal.squeeze(),
+                                          IsInCollision)
+
             if target_reached:
-                return pA.extend(reversed(pB)), 0
+                pA.extend(reversed(pB))
+                return pA, 0
         return noPath, 0
 
     def lvc(self, path, IsInCollision):
@@ -145,6 +220,6 @@ class MPNetPlan(MPnetBase):
         new_path = []
 
         for i in range(0, len(path)):
-            if not IsInCollision(path[i].numpy()):
+            if not IsInCollision(path[i]):
                 new_path.append(path[i])
         return new_path
