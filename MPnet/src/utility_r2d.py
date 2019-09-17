3a979c73793908c406fb001c71e5e03a5bb06efc
jacobjj <jjj025@ucsd.edu>
2019-09-17 15:23:58 -0700

    Normalize and denormalize functions

--- MPnet/src/utility_r2d.py
+++ MPnet/src/utility_r2d.py
@@ -3,6 +3,8 @@ from torch.autograd import Variable
 import copy
 import numpy as np
 import time
+
+
 def normalize(x, bound, time_flag=False):
     # normalize to -1 ~ 1  (bound can be a tensor)
     #return x
@@ -11,15 +13,18 @@ def normalize(x, bound, time_flag=False):
     if len(x[0]) != len(bound):
         # then the proceding is obstacle
         # don't normalize obstacles
-        x[:,:-2*len(bound)] = x[:,:-2*len(bound)] / bound[0]
-        x[:,-2*len(bound):-len(bound)] = x[:,-2*len(bound):-len(bound)] / bound
-        x[:,-len(bound):] = x[:,-len(bound):] / bound
+        x[:, :-2 * len(bound)] = x[:, :-2 * len(bound)] / bound[0]
+        x[:, -2 *
+          len(bound):-len(bound)] = x[:, -2 * len(bound):-len(bound)] / bound
+        x[:, -len(bound):] = x[:, -len(bound):] / bound
     else:
         x = x / bound
     if time_flag:
         return x, time.time() - time_0
     else:
         return x
+
+
 def unnormalize(x, bound, time_flag=False):
     # normalize to -1 ~ 1  (bound can be a tensor)
     # x only one dim
@@ -29,9 +34,10 @@ def unnormalize(x, bound, time_flag=False):
     if len(x) != len(bound):
         # then the proceding is obstacle
         # don't normalize obstacles
-        x[:,:-2*len(bound)] = x[:,:-2*len(bound)] * bound[0]
-        x[:,-2*len(bound):-len(bound)] = x[:,-2*len(bound):-len(bound)] * bound
-        x[:,-len(bound):] = x[:,-len(bound):] * bound
+        x[:, :-2 * len(bound)] = x[:, :-2 * len(bound)] * bound[0]
+        x[:, -2 *
+          len(bound):-len(bound)] = x[:, -2 * len(bound):-len(bound)] * bound
+        x[:, -len(bound):] = x[:, -len(bound):] * bound
     else:
         x = x * bound
     if time_flag:
