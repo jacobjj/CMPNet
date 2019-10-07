e7968e2191a543182094019552a3f0ea1b4b0f08
jacobjj <jjj025@ucsd.edu>
2019-10-07 16:25:05 -0700

    added model parameterizing dubins curves

---
+++ MPnet/Model/model_dubins.py
@@ -0,0 +1,50 @@
+""" A nn parameterized by dubins curve"""
+import torch.nn as nn
+import torch
+import numpy as np
+
+def normalize_angle(z):
+    """
+    A function to wrap around -1 and 1
+    """
+    return (z + 1) % (2) - 1
+
+class DubinsPathGenerator(nn.Module):
+    def __init__(self, input_size):
+        super(DubinsPathGenerator, self).__init__()
+
+        self.fc = nn.Sequential(
+            nn.Linear(input_size, 256),
+            nn.PReLU(),
+            nn.Dropout(),
+            nn.Linear(256, 128),
+            nn.PReLU(),
+            nn.Dropout(),
+            nn.Linear(128, 64),
+            nn.PReLU(),
+            nn.Dropout(),
+            nn.Linear(64, 32),
+            nn.PReLU(),
+            nn.Linear(32, 3),
+        )
+
+        def LeftTurn(self, x, beta):
+            return torch.Tensor([
+                x[:, 0] + torch.sin(x[:, 2] + beta) - torch.sin(x[:, 2]),
+                x[:, 1] - torch.cos(x[:, 2] + beta) + torch.cos(x[:, 2]),
+                normalize_angle(beta + x[:, 2]),
+            ])
+
+        def RightTurn(self, x, alpha):
+            return torch.Tensor([
+                x[:, 0] - torch.sin(x[:, 2] - alpha) + torch.sin(x[:, 2]),
+                x[:, 1] + torch.cos(x[:, 2] - alpha) - torch.cos(x[:, 2]),
+                normalize_angle(alpha - x[:, 2]),
+            ])
+
+        def StraightTrun(self, x, gamma):
+            return torch.Tensor([
+                x[:, 0] + gamma * torch.cos(x[:, 2]),
+                x[:, 1] + gamma * torch.sin(x[:, 2]),
+                x[:, 2],
+            ])
