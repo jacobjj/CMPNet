7786c35b27298609181c58c66d08302fa5abee58
jacobjj <jjj025@ucsd.edu>
2019-11-05 09:44:24 -0800

    changed normalization method

--- MPnet/src/utility_r2d.py
+++ MPnet/src/utility_r2d.py
@@ -13,10 +13,10 @@ def normalize(x, bound, time_flag=False):
     if len(x[0]) != len(bound):
         # then the proceding is obstacle
         # don't normalize obstacles
-        x[:, :-2 * len(bound)] = x[:, :-2 * len(bound)] / bound[0]
-        x[:, -2 *
-          len(bound):-len(bound)] = x[:, -2 * len(bound):-len(bound)] / bound
-        x[:, -len(bound):] = x[:, -len(bound):] / bound
+        x[..., :-2 * len(bound)] = x[..., :-2 * len(bound)] / bound[0]
+        x[..., -2 *
+          len(bound):-len(bound)] = x[..., -2 * len(bound):-len(bound)] / bound
+        x[..., -len(bound):] = x[..., -len(bound):] / bound
     else:
         x = x / bound
     if time_flag:
