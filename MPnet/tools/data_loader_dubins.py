4d404b7f32576eea48c5a581af9fc0f1f527af12
jacobjj <jjj025@ucsd.edu>
2019-11-05 09:41:42 -0800

    added pytorch dataloader for faster loading

--- MPnet/tools/data_loader_dubins.py
+++ MPnet/tools/data_loader_dubins.py
@@ -4,7 +4,10 @@ import numpy as np
 import dubins
 
 import torch
+from torch.utils.data import DataLoader
 import torch.utils.data
+import os
+import re
 
 from bc_gym_planning_env.envs.mini_env import RandomMiniEnv
 
@@ -39,45 +42,51 @@ def primitive2word(primitive_length, primitive_type, d):
 
 
 class DubinsDataset(torch.utils.data.Dataset):
-    def __init__(self, folder_loc):
+    def __init__(self, folder_loc, numSamples):
         self.folder_loc = folder_loc
+        self.numSamples = numSamples
+        self.inputs = np.zeros((numSamples, 6))
+        self.targets = np.zeros((numSamples, 9))
+        self.obs = np.zeros((numSamples, 1, 122, 122))
+        i = 0
+        done = False
+
+        trajFolder = osp.join(folder_loc, 'traj')
+        seeds = []
+
+        for entry in os.listdir(trajFolder):
+            if '.npy' in entry:
+                s = int(re.findall(r'\d+', entry)[0])
+                seeds.append(s)
+
+        DataSet = DubinsIterDataset(trajFolder, seeds)
+        Data = DataLoader(DataSet, num_workers=5)
+
+        if not seeds:
+            raise ValueError("{} - Not a valid folder".format(trajFolder))
+        # Load point cloud, points and target information
+        count = 0
+        for data in Data:
+            if len(data['obs'])==0:
+                count +=1
+                continue
+
+            numSubSamples = data['obs'].shape[1]
+
+            stop_iter = min(i+numSubSamples, numSamples)
+            self.obs[i:stop_iter, ...] = data['obs'].squeeze(0)[:stop_iter-i,...]
+            self.inputs[i:stop_iter, ...] = data['inputs'].squeeze(0)[:stop_iter-i,...]
+            self.targets[i:stop_iter, ...] = data['targets'].squeeze(0)[:stop_iter-i,...]
+            i = stop_iter
+            if i == numSamples:
+                done = True
+                break
 
     def __len__(self):
-        return 4000
+        return self.numSamples
 
     def __getitem__(self, idx):
-        env = RandomMiniEnv(
-            turn_off_obstacles=False,
-            draw_new_turn_on_reset=False,
-            iteration_timeout=1200,
-            seed=idx,
-            goal_spat_dist=0.05,
-        )
-        obs = env.reset()
-        costmap = obs.costmap
-        costmap_data = costmap.get_data()
-        costmap_dim = costmap_data.shape
-        traj = np.load(osp.join(self.folder_loc, 'traj_{}.npy'.format(idx)))
-        samples = traj.shape[0] - 1
-        pixel_ind = [
-            costmap.world_to_pixel(coordinate[:2]) for coordinate in traj[:-1]
-        ]
-        obs = np.ones((samples, costmap_dim[0] * 2, costmap_dim[1] * 2))
-        inputs = np.zeros((samples, 6))
-        targets = np.zeros((samples, 9))
-        for j, _ in enumerate(traj[:-1]):
-            x_0, y_0 = costmap_dim[1] - pixel_ind[j][1], costmap_dim[
-                0] - pixel_ind[j][0]
-            obs[j, x_0:x_0 + costmap_dim[1], y_0:y_0 +
-                costmap_dim[0]] = costmap_data / 254
-            inputs[j, :] = np.concatenate((traj[j], traj[-1]))
-            path = dubins.shortest_path(tuple(traj[j]), tuple(traj[j + 1]),
-                                        0.6)
-            primitive_length = [path.segment_length(i) for i in range(3)]
-            primitive_type = path.path_type()
-            s = primitive2word(primitive_length, primitive_type, d=0.6)
-            targets[j, :] = s.ravel()
-        return {'obs': obs, 'inputs': inputs, 'targets': targets}
+        return self.obs[idx, ...], self.inputs[idx, ...], self.targets[idx, ...]
 
 
 class DubinsIterDataset(torch.utils.data.IterableDataset):
@@ -140,4 +149,8 @@ class DubinsIterDataset(torch.utils.data.IterableDataset):
             primitive_type = path.path_type()
             s = primitive2word(primitive_length, primitive_type, d=0.6)
             targets[j, :] = s.ravel()
-        return {'obs': obs, 'inputs': inputs, 'targets': targets}
+        return {
+            'obs': np.array(obs, dtype=np.float32),
+            'inputs': np.array(inputs, dtype=np.float32),
+            'targets': np.array(targets, dtype=np.float32)
+        }
