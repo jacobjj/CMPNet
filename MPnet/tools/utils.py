f21ec4f26798229e87cdeb7d5f59fe5d7ad95874
jacobjj <jjj025@ucsd.edu>
2019-11-18 17:28:00 -0800

    Generate coordinate points in 3D space

--- MPnet/tools/utils.py
+++ MPnet/tools/utils.py
@@ -52,18 +52,44 @@ def word2primitive(word, start, d):
     F = [RightTurn, LeftTurn, StraightTurn]
     temp_word = torch.abs(word.clone())
     for i in [0, 1, 3, 4, 6, 7]:
-        temp_word[i] = temp_word[i] * np.pi * d
-        word[i] = word[i] * np.pi
-    word = word.reshape((3, 3))
-    temp_word = temp_word.reshape((3, 3))
-    x_i = torch.Tensor([0, 0, start[2]])
+        temp_word[:, i] = temp_word[:, i] * np.pi * d
+        word[:, i] = word[:, i] * np.pi
+    # word = word.reshape((3, 3))
+    # temp_word = temp_word.reshape((3, 3))
+    # x_i = torch.Tensor([0, 0, start[2]])
+    x_i = torch.zeros_like(start)
+    x_i[:, 2] += start[:, 2]
     for i in range(3):
-        _, index = torch.max(temp_word[i, :], dim=0)
-        v = word[i, index]
-        if index == 2:
-            v = v / d
-        x_i = F[index](x_i, v)
-    start[0] += x_i[0] * d
-    start[1] += x_i[1] * d
-    start[2] = normalize_angle(start[2] + x_i[2])
+        _, index = torch.max(temp_word[:, i * 3:(i + 1) * 3], dim=1)
+        index += 3 * i
+        for n in range(word.shape[0]):
+            v = word[n, index[n]]
+            if index[n] == 5:
+                v = v / d
+            x_i[n, :] = F[index[n] % 3](x_i[n, :], v)
+    start[:, :2] += x_i[:, :2] * d
+    start[:, 2] = normalize_angle(start[:, 2] + x_i[:, 2])
     return start
+
+
+def word2type(word, d):
+    """
+    Returns the class corresponding to the word
+    """
+    temp_word = torch.abs(word.clone())
+    for i in [0, 1, 3, 4, 6, 7]:
+        temp_word[:, i] = temp_word[:, i] * np.pi * d
+        word[:, i] = word[:, i] * np.pi
+    curve_classes = ['LSL', 'LSR', 'RSL', 'RSR', 'RLR', 'LRL']
+    current_class = ''
+    for i in range(3):
+        _, index = torch.max(temp_word[:, i * 3:(i + 1) * 3], dim=1)
+        if index==0 :
+            current_class += 'R'
+        elif index==1:
+            current_class += 'L'
+        else:
+            current_class += 'S'
+
+    return curve_classes.index(current_class)
+
