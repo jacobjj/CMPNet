46c51084b09b9bf6dd9b48197c8d8abeed24b984
jacobjj <jjj025@ucsd.edu>
2019-11-22 18:04:39 -0800

    change model

--- MPnet/scripts/plan_tricycle.py
+++ MPnet/scripts/plan_tricycle.py
@@ -20,20 +20,8 @@ def steerTo_env(start, goal, IsInCollision):
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    s = 89
-    env = RandomMiniEnv(draw_new_turn_on_reset=False,
-                        seed=s,
-                        goal_spat_dist=0.05)
-    costmap = env._env._state.costmap
-    robot = env._env._robot
-    IsInCollision_env = lambda pose: pose_collides(
-        pose[0],
-        pose[1],
-        pose[2],
-        robot,
-        costmap,
-    )
-
+    # s = 304299
+    s = 0
     network_param = {
         'normalize': normalize,
         'denormalize': unnormalize,
@@ -42,39 +30,70 @@ if __name__ == "__main__":
         'worldSize': [2.75, 2.75, np.pi],
         'AE': voxelNet,
         'MLP': model.MLP,
-        'modelPath': 'data/MPnet_tricycle/points/Adam_lr_3_minus_4_centerObs'
+        'modelPath': 'data/MPnet_tricycle/points/Adam_lr_3_minus_4_centerObs_relativeTarget'
     }
 
     MPNetPlan_obj = MPNetPlan(
         modelFile=
-        'data/MPnet_tricycle/points/Adam_lr_3_minus_4_centerObs/mpnet_epoch_99.pkl',
+        'data/MPnet_tricycle/points/Adam_lr_3_minus_4_centerObs_relativeTarget/mpnet_epoch_19.pkl',
         steerTo=steerTo,
         **network_param,
     )
-    start_node = env._env._state.pose
-    goal_node = env._env._state.original_path[-1]
-    # Shifted costmap
-    observation = env.reset()
-    costmap = observation.costmap
-    pointcloud = generate_point_cloud(s)
-    path = MPNetPlan_obj.getPath(
-        IsInCollision_env,
-        start_node,
-        goal_node,
-        costmap,
-        pointcloud,
-    )
 
-    if path:
-        plt.figure()
-        plt.scatter(pointcloud[:, 0], pointcloud[:, 1])
-        for p in path:
-            plt.scatter(p[0], p[1], marker='x', color='r')
-        for i, _ in enumerate(path[:-1]):
-            p = dubins.shortest_path(tuple(path[i].numpy()),
-                                     tuple(path[i + 1].numpy()), 0.6)
-            config, _ = p.sample_many(0.1)
-            config = np.array(config)
-            plt.plot(config[:, 0], config[:, 1])
+    success = 0
+    obstacle_path = 0
+    for s in range(0,100):
+        env = RandomMiniEnv(draw_new_turn_on_reset=False,
+                            seed=s,
+                            goal_spat_dist=0.05)
+        observation = env.reset()
+        costmap = observation.costmap
+        robot = env._env._robot
+        IsInCollision_env = lambda pose: pose_collides(
+            pose[0],
+            pose[1],
+            pose[2],
+            robot,
+            costmap,
+        )
 
-        plt.show()
+        start_node = env._env._state.pose
+        goal_node = env._env._state.original_path[-1]
+        # Shifted costmap
+        observation = env.reset()
+        costmap = observation.costmap
+        pointcloud = generate_point_cloud(s)
+        # traj = np.load('data/dubinsCar/traj/traj_{}.npy'.format(s))
+        # print("Start : {}, trajectory :{}".format(traj[0], traj[-1]))
+        path = MPNetPlan_obj.getPath(
+            IsInCollision_env,
+            start_node,
+            goal_node,
+            costmap,
+            pointcloud,
+        )
+
+        if len(path)==2:
+            success += 1
+            continue
+
+        render = False
+        if path:
+            success += 1
+            obstacle_path +=1
+        if path and render:
+            plt.figure()
+            plt.scatter(pointcloud[:, 0], pointcloud[:, 1])
+            for p in path:
+                plt.scatter(p[0], p[1], marker='x', color='r')
+            for i, _ in enumerate(path[:-1]):
+                p = dubins.shortest_path(tuple(path[i].numpy()),
+                                         tuple(path[i + 1].numpy()), 0.6)
+                config, _ = p.sample_many(0.1)
+                config = np.array(config)
+                plt.plot(config[:, 0], config[:, 1])
+
+            plt.pause(1)
+            plt.close()
+
+    print("Successful Paths : {}, Successful MPnet Paths : {} ".format(success, obstacle_path))
